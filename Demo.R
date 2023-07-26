b034d1e60250f9b84dea8c7604d08a7f2b4ccfd6
Rin Leng <64882208+chlengcold@users.noreply.github.com>
2023-07-26 16:27:49 +0800

    Update Demo.R

--- Demo.R
+++ Demo.R
@@ -9,13 +9,18 @@ library(tidyverse)
 
 # The data shall be scaled as 0 -- J-1 in a J-point item.
 y = read.csv('Data_BFI.csv') %>% filter(Condition==3) %>% select(!all_of('Condition'))
-# number of scale
+# J-point scale
 J= 5 
 # number of items in each dimension
+## the example is five dimensions containing 10 items, respectively
 pois = rep(10,5)
 # the corresponding dimension for each item
+## the example is items 1-9 are in dimension 1; items 10-19 are in dimension 2; items 21-29 are in dimension 3; items 31-39 are in dimension 4; items 41-49 are in dimension 5.
 t_pois = rep(1:5,each=10)
 
+# iter: total number of iterations
+# burnin: number of burn-in
+# thin: number of thining
 mod = RDT(y,J,pois=pois,t_pois=t_pois,nchain=3,iters=21000,burnin=7000,thin=14)
 
 # EAP estimates

12ed0b81258f34bc896ee11750c361e3569cb4bd
Rin Leng <64882208+chlengcold@users.noreply.github.com>
2023-08-01 11:07:42 +0800

    Update Demo.R

--- Demo.R
+++ Demo.R
@@ -6,11 +6,10 @@ library(foreach)
 library(doParallel)
 library(ranger)
 library(tidyverse)
+library(BNSP)
 
 # The data shall be scaled as 0 -- J-1 in a J-point item.
 y = read.csv('Data_BFI.csv') %>% filter(Condition==3) %>% select(!all_of('Condition'))
-# J-point scale
-J= 5 
 # number of items in each dimension
 ## the example is five dimensions containing 10 items, respectively
 pois = rep(10,5)
@@ -18,10 +17,19 @@ pois = rep(10,5)
 ## the example is items 1-9 are in dimension 1; items 10-19 are in dimension 2; items 21-29 are in dimension 3; items 31-39 are in dimension 4; items 41-49 are in dimension 5.
 t_pois = rep(1:5,each=10)
 
+# J: J-point scale
+# nchain: number of chains, must be larger than 1
 # iter: total number of iterations
 # burnin: number of burn-in
 # thin: number of thining
-mod = RDT(y,J,pois=pois,t_pois=t_pois,nchain=3,iters=21000,burnin=7000,thin=14)
+T1 = Sys.time()
+mod = RDT(y,J=5,pois=pois,t_pois=t_pois,nchain=3,iters=21000,burnin=7000,thin=14)
+Sys.time() - T1
+
+#Re-train
+T2 = Sys.time()
+mod = RDT(y,J=5,pois=pois,t_pois=t_pois,nchain=3,iters=15000,burnin=5000,thin=10,ReTrain=T,mod=mod)
+Sys.time() - T2
 
 # EAP estimates
 ## Alpha, Gamma, Delta, Omega, Tau, ThetaR, ThetaD, ThetaT, Sigma11, Sigma22
@@ -29,12 +37,12 @@ mod$Estimate$Alpha
 
 # R-Square
 ## Alpha, Gamma, Delta, Omega, Tau, ThetaR, ThetaD, ThetaT
-mod$RSquare$Alpha
+mod$RSquare$Delta
 
 # MCMC plot
 ## Alpha, Gamma, Delta, Omega, Tau, ThetaD, ThetaT
 ### i: item or individual
-plotRDTMCMC(mod, x = 'Alpha', i=1)
+plotRDTMCMC(mod, x = 'Omega', i=1)
 ## ThetaR
 ### d: dimension
 plotRDTMCMC_M(mod, x = 'ThetaR', d=1, i=1)
